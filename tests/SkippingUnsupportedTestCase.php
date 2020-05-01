7288a1d2ac1494a7bcfd80106ce32e8c0f53c8ac
Lctrs <jerome@prmntr.me>
2020-05-01 21:23:43 +0200

    Fix incompatibilities with PHPUnit 8

--- tests/SkippingUnsupportedTestCase.php
+++ tests/SkippingUnsupportedTestCase.php
@@ -4,60 +4,18 @@ namespace Behat\Mink\Tests\Driver;
 
 use Behat\Mink\Exception\UnsupportedDriverActionException;
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use PHPUnit\Runner\Version;
 
-if (class_exists('PHPUnit\Runner\Version') && version_compare(Version::id(), '6.0.0', '>=')) {
+class SkippingUnsupportedTestCase extends BaseTestCase
+{
     /**
-     * Implementation of the skipping for UnsupportedDriverActionException for PHPUnit 6+.
-     *
-     * This code should be moved back to \Behat\Mink\Tests\Driver\TestCase when dropping support for
-     * PHP 5.6 and older, as PHPUnit 4 and 5 won't be needed anymore.
-     *
      * @internal
      */
-    class SkippingUnsupportedTestCase extends BaseTestCase
+    protected function onNotSuccessfulTest(\Throwable $t): void
     {
-        protected function onNotSuccessfulTest(\Throwable $e)
-        {
-            if ($e instanceof eUnsupportedDriverActionException) {
-                $this->markTestSkipped($e->getMessage());
-            }
-
-            parent::onNotSuccessfulTest($e);
+        if ($t instanceof UnsupportedDriverActionException) {
+            $this->markTestSkipped($t->getMessage());
         }
-    }
-} elseif (version_compare(\PHPUnit_Runner_Version::id(), '5.0.0', '>=')) {
-    /**
-     * Implementation of the skipping for UnsupportedDriverActionException for PHPUnit 5.
-     *
-     * @internal
-     */
-    class SkippingUnsupportedTestCase extends \PHPUnit_Framework_TestCase
-    {
-        protected function onNotSuccessfulTest($e)
-        {
-            if ($e instanceof UnsupportedDriverActionException) {
-                $this->markTestSkipped($e->getMessage());
-            }
 
-            parent::onNotSuccessfulTest($e);
-        }
-    }
-} else {
-    /**
-     * Implementation of the skipping for UnsupportedDriverActionException for PHPUnit 4.
-     *
-     * @internal
-     */
-    class SkippingUnsupportedTestCase extends \PHPUnit_Framework_TestCase
-    {
-        protected function onNotSuccessfulTest(\Exception $e)
-        {
-            if ($e instanceof UnsupportedDriverActionException) {
-                $this->markTestSkipped($e->getMessage());
-            }
-
-            parent::onNotSuccessfulTest($e);
-        }
+        parent::onNotSuccessfulTest($t);
     }
 }
